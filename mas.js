ea711dfc8193147b2bb27ce5fb6e11792b672644
KaykyPE81 <96851498+KaykyPE81@users.noreply.github.com>
2022-01-30 14:21:13 -0300

    commit inicial
    Mading up pages for tests

---
+++ mas.js
@@ -0,0 +1,38 @@
+alert('ATENÇÃO! Esse site ainda está em desenvolvimento, pedimos desculpas por qualquer bug que houver. A Banda Ki Baby agradeçe!');
+
+/*
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides-fade");
+/*  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+
+  slides[slideIndex-1].style.display = "block";
+/*  dots[slideIndex-1].className += "active";
+}*/
+
+
+//Linking respective images with yours websites
+function linkarInsta() {
+  window.open("https://www.instagram.com/bandaky.baby.ofc/", '_self');
+}
+
+function linkarYt(){
+  window.open("https://www.youtube.com/channel/UCEEGffAC1_1z3aJY9XSyfBQ/videos?app=desktop", '_self');
+}
+function linkarMp3(){
+  window.open("https://www.palcomp3.com.br/KatiaSantoseBandaKyBabyForro/", "_self");
+}
